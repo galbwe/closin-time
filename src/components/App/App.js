0c5354c0e6832a77a9a3bae0b4f9fa27e038b637
Wes Galbraith <galbwe92@gmail.com>
2021-07-01 00:17:04 -0600

    add grommet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import { Grommet } from 'grommet';
 
 import CountdownTimer from '../CountdownTimer/CountdownTimer';
 import ClosingTimeDisplay from '../ClosingTimeDisplay/ClosingTimeDisplay';
+
+const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '14px',
+        height: '20px',
+      },
+    },
+  };
 
 
 const getCurrentTime = () => {
@@ -30,17 +41,30 @@ const App = () => {
         seconds: 0,
     };  // 5 pm
 
-    return <CountdownTimer currentTime={{hours: 14, minutes: 23, seconds: 42}} target={closingTime} />
 
-    // return (
-    //     // if currentTime is before 5pm, display a countdown until closin' time
-    //     // if currentTime is after 5pm, let the user know that it is closin' time
-    //     currentTime.hours < closingTime.hours ? (
-    //         <CountdownTimer currentTime={currentTime} target={closingTime} />
-    //     ) : (
-    //         <ClosingTimeDisplay />
-    //     )
-    // )
+    return (
+        <Grommet theme={theme}>
+
+            {/* TODO: remove dummy timers and uncomment conditional rendering  */}
+          <CountdownTimer currentTime={{hours: 14, minutes: 23, seconds: 42}} target={closingTime} />
+
+          <CountdownTimer currentTime={{hours: 0, minutes: 23, seconds: 42}} target={closingTime} />
+
+          <CountdownTimer currentTime={{hours: 0, minutes: 0, seconds: 0}} target={closingTime} />
+
+          <CountdownTimer currentTime={{hours: 12, minutes: 0, seconds: 0}} target={closingTime} />
+
+
+          {/* if currentTime is before 5pm, display a countdown until closin' time
+          if currentTime is after 5pm, let the user know that it is closin' time */}
+         {/* {currentTime.hours < closingTime.hours ? (
+            <CountdownTimer currentTime={currentTime} target={closingTime} />
+         ) : (
+             <ClosingTimeDisplay />
+         )} */}
+
+        </Grommet>
+    )
 }
 
 export default App
--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -1,20 +1,38 @@
 import React from 'react'
+import { Box } from 'grommet';
 
 const convertToSeconds = ({hours, minutes, seconds}) => {
     return 3600 * hours + 60 * minutes + seconds
 }
 
+const formatWithLeadingZero = (x) => {
+    if (x < 10) {
+        return `0${x}`;
+    }
+    return `${x}`;
+}
+
 const CountdownTimer = ({currentTime, target}) => {
+
     let secondsToClosingTime = convertToSeconds(target) - convertToSeconds(currentTime);
+
     let hours = Math.floor(secondsToClosingTime / 3600);
     secondsToClosingTime = secondsToClosingTime % 3600;
     let minutes = Math.floor(secondsToClosingTime / 60);
     let seconds = secondsToClosingTime % 60;
+    let countdown = `${formatWithLeadingZero(hours)}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
+
+    let displayHour = currentTime.hours % 12 === 0 ? 12 : currentTime.hours % 12;
+    displayHour = formatWithLeadingZero(displayHour);
+    let displayMinutes = formatWithLeadingZero(currentTime.minutes);
+    let displaySeconds = formatWithLeadingZero(currentTime.seconds);
+    let civilianTimeSuffix = currentTime.hours < 12 ? 'AM' : 'PM';
 
     return (
-        <div>
-            <p>{hours}:{minutes}:{seconds} until Closin' Time</p>
-        </div>
+        <Box>
+            <p>It's {displayHour}:{displayMinutes}:{displaySeconds} {civilianTimeSuffix}</p>
+            <p>{countdown} until Closin' Time</p>
+        </Box>
     )}
 
 export default CountdownTimer
